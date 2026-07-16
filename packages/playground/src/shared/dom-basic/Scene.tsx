a7dd587be7be6cef8ea036439db7005a446f5795
ashthornton <ashthornton0@gmail.com>
2026-07-16 18:28:41 +0200

    fix: remove window-relative coords from dom-basic example to avoid confusion when using with remote editor

--- packages/playground/src/shared/dom-basic/Scene.tsx
+++ packages/playground/src/shared/dom-basic/Scene.tsx
@@ -1,6 +1,5 @@
-import React, {useEffect, useRef} from 'react'
+import React, { useRef} from 'react'
 import type {CSSProperties} from 'react'
-import {types} from '@theatre/core'
 import type {IProject} from '@theatre/core'
 import {Box3D, BoxSize} from './Box3D'
 
@@ -19,28 +18,9 @@ export const Scene: React.FC<{project: IProject}> = ({project}) => {
   const containerRef = useRef<HTMLDivElement>(null!)
   const sheet = project.sheet('DOM')
 
-  useEffect(() => {
-    const container = containerRef.current!
-    const sheetObj = sheet.object('Container', {
-      perspective: types.number(
-        Math.max(window.innerWidth, window.innerHeight),
-        {range: [0, 2000]},
-      ),
-      originX: types.number(50, {range: [0, 100]}),
-      originY: types.number(50, {range: [0, 100]}),
-    })
-    const unsubscribe = sheetObj.onValuesChange((values: any) => {
-      container.style.perspective = `${values.perspective}px`
-      container.style.perspectiveOrigin = `${values.originX}% ${values.originY}%`
-    })
-    return () => {
-      unsubscribe()
-    }
-  }, [])
-
   const padding = 100
-  const right = window.innerWidth - padding - BoxSize
-  const bottom = window.innerHeight - padding - BoxSize
+  const right = padding - BoxSize
+  const bottom = padding - BoxSize
 
   return (
     <div ref={containerRef} style={SceneCSS}>
